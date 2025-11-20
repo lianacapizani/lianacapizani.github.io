892f2b503b048ec45294679531adbe026d2f60bd
Liana Karla Capizani <lianacapizani@gmail.com>
2025-11-20 13:10:44 -0300

    ajuste icone

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,6 +9,7 @@ import html5Icon from '@iconify/icons-logos/html-5';
 import css3Icon from '@iconify/icons-logos/css-3';
 import gitIcon from '@iconify/icons-logos/git-icon';
 import figmaIcon from '@iconify/icons-logos/figma';
+import nextIcon from "@iconify/icons-logos/nextjs-icon";
 
 export default function Skills() {
   return (
@@ -21,11 +22,12 @@ export default function Skills() {
         <div className={styles.column}>
           <h2>Frontend Development</h2>
           <div className={styles.skillItems}>
-            <div><Icon icon={reactIcon} /> <span>ReactJS</span></div>
-            <div><Icon icon={javascriptIcon} /> <span>JavaScript (ES6+)</span></div>
-            <div><Icon icon={typescriptIcon} /> <span>TypeScript</span></div>
             <div><Icon icon={html5Icon} /> <span>HTML5</span></div>
             <div><Icon icon={css3Icon} /> <span>CSS3</span></div>
+            <div><Icon icon={javascriptIcon} /> <span>JavaScript (ES6+)</span></div>
+            <div><Icon icon={typescriptIcon} /> <span>TypeScript</span></div>
+            <div><Icon icon={reactIcon} /> <span>React</span></div>
+            <div><Icon icon={nextIcon} /> <span>Next.JS</span></div>
           </div>
         </div>
 
